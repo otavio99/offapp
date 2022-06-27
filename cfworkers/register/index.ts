751ddfa80aa3027056b22aa97da6b56894c0a229
Otavio Nathanael <otavio.nathanael@gmail.com>
2022-06-27 19:03:53 -0400

    finishing jwt verification

--- cfworkers/register/index.ts
+++ cfworkers/register/index.ts
@@ -43,15 +43,17 @@ function handleOptions(request) {
 
 async function handleGet(request) {
   const token = request.headers.get('Authorization');
-  const { jwtVerify } = require('jose')
+  const { jwtVerify } = require('jose');
+  const { createRemoteJWKSet } = require('jose');
   const keypromise = await fetch("https://dev-qy-iakti.us.auth0.com/.well-known/jwks.json");
-  const keyjson = await keypromise.json();
-  const response = jwtVerify(token, keyjson["keys"][0]["x5c"], {
-    issuer: 'dev-qy-iakti.us.auth0.com',
-    audience: 'eEBy63jCkRcKBsKpAfOWit5hg8JgZzpG'
+  const jwks = createRemoteJWKSet(new URL('https://dev-qy-iakti.us.auth0.com/.well-known/jwks.json'));
+  const { payload, protectedHeader } = await jwtVerify(token.replaceAll('"',''), jwks, {
+    iss: 'dev-qy-iakti.us.auth0.com/',
+    aud: 'eEBy63jCkRcKBsKpAfOWit5hg8JgZzpG'
   })
 
-  console.log(response)
+  console.log(payload.nickname)
+
   return new Response(JSON.stringify(meaning), {
     headers: {
       ...corsHeaders,
