e087427667ca514d114ea6c1f51c65b44e8531e3
Otavio Nathanael <otavio.nathanael@gmail.com>
2022-06-27 17:47:35 -0400

    adding jose jwt verification part 1

--- cfworkers/register/index.ts
+++ cfworkers/register/index.ts
@@ -5,5 +5,56 @@ addEventListener("fetch", event => {
 })
 
 async function handleRequest(request) {
-  return new Response(meaning)
+  if (request.method === "OPTIONS") {
+    return handleOptions(request)
+  } else if (request.method === "GET") {
+    return handleGet(request)
+  } else {
+    return new Response(null, {
+      status: 405,
+      statusText: "Method Not Allowed",
+    })
+  }
+}
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, HEAD, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type,Authorization",
+}
+
+function handleOptions(request) {
+  if (request.headers.get("Origin") !== null &&
+      request.headers.get("Access-Control-Request-Method") !== null &&
+      request.headers.get("Access-Control-Request-Headers") !== null) {
+    // Handle CORS pre-flight request.
+    return new Response(null, {
+      headers: corsHeaders
+    })
+  } else {
+    // Handle standard OPTIONS request.
+    return new Response(null, {
+      headers: {
+        "Allow": "GET, HEAD, POST, OPTIONS",
+      }
+    })
+  }
+}
+
+async function handleGet(request) {
+  const token = request.headers.get('Authorization');
+  const { jwtVerify } = require('jose')
+  const keypromise = await fetch("https://dev-qy-iakti.us.auth0.com/.well-known/jwks.json");
+  const keyjson = await keypromise.json();
+  const response = jwtVerify(token, keyjson["keys"][0]["x5c"], {
+    issuer: 'dev-qy-iakti.us.auth0.com',
+    audience: 'eEBy63jCkRcKBsKpAfOWit5hg8JgZzpG'
+  })
+
+  console.log(response)
+  return new Response(JSON.stringify(meaning), {
+    headers: {
+      ...corsHeaders,
+    }
+  })
 }
